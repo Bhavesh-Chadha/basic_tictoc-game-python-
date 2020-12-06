58f8c371ba3be5f5e70d75d3186f572bc7997373
Bhavesh-Chadha <bhaveshchadha14@gmail.com>
2020-12-06 20:45:32 +0530

    updated for colledge project

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,9 @@
+print("Enter a name for the X player:")
+name1= input()
+print("\nEnter a name for the Y player e r:")
+name2= input()
+print("Game Start:")
+
 a = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
 
 print("---------") 
@@ -7,13 +13,16 @@ for i in range(0,3):
   print('\n|',a[i][0],a[i][1],a[i][2],'|')
   
 print("\n---------")
+print("Who plays first?",name1,"or", name2)
 
+input_name= input()
+if (input_name != name1 and input_name != name2):
+    print(input_name,"is not a registered player")
 
-
-o ='X'
+o ='row'
 k = 0
-nx = 0
-ny = 0
+nrow = 0
+ncolumn   = 0
 
 while(k == 0):
     
@@ -21,41 +30,41 @@ while(k == 0):
       
     print("Draw")
     break
-  if(a[0][0] == a[0][1] ==a[0][2]   == 'X' or a[0][0] == a[1][1] == a[2][2] == 'X' or a[0][0] == a[1][0] == a[2][0] == 'X'or a[1][1] == a[0][0] == a[2][0] == 'X'or a[2][2] == a[2][1] == a[2][0] == 'X'or a[1][0] == a[1][1] == a[1][2] == 'X'or a[0][2] == a[1][2] == a[2][2] == 'X'or a[2][1] == a[0][1] == a[1][1] == 'X') :
-        print("X wins") 
+  if(a[0][0] == a[0][1] ==a[0][2]   == 'row' or a[0][0] == a[1][1] == a[2][2] == 'row' or a[0][0] == a[1][0] == a[2][0] == 'row'or a[1][1] == a[0][0] == a[2][0] == 'row'or a[2][2] == a[2][1] == a[2][0] == 'row'or a[1][0] == a[1][1] == a[1][2] == 'row'or a[0][2] == a[1][2] == a[2][2] == 'row'or a[2][1] == a[0][1] == a[1][1] == 'row') :
+        print(name1, "wins") 
         break  
 
 
   elif (a[0][0] ==a[0][1] ==a[0][2]   == 'O' or a[0][0] == a[1][1] == a[2][2] == 'O' or a[0][0] == a[1][0] == a[2][0] == 'O'or a[1][1] == a[0][0] == a[2][0] == 'O'or a[2][2] == a[2][1] == a[2][0] == 'O'or a[1][0] == a[1][1] == a[1][2] == 'O'or a[0][2] == a[1][2] == a[2][2] == 'O'or a[2][1] == a[0][1] == a[1][1] == 'O') :    
-    print("O wins")
+    print(name2," wins")
     break
   
 
       
 
-  x,y = input("Enter the coordinates: ").split()
-  x = int(x)
-  y = int(y)
-  if x <=3 and x>= 1 and y <= 3 and y>=1:
-    if a[x-1][abs(y-3)] != ' ' :
+  row, column = input("Enter the coordinates: ").split()
+  row = int(row)
+  column = int(column)
+  if row <=3 and  row >= 1 and column <= 3 and column >= 1:
+    if a[row-1][abs(column - 3)] != ' ' :
       print("This cell is occupied! Choose another one!")
       continue
     else :
        
-      a[x-1][abs(y-3)] = o
-      if o == 'X' :
+      a[row - 1][abs(column - 3)] = o
+      if o == 'row' :
            o = '0'
-           nx += 1  
+           nrow += 1  
       else :
-            o = 'X'
-            ny += 1   
+            o = 'row'
+            ncolumn   += 1   
        
      # break
-  elif x >=3 and x<= 1 or y >= 3 or y<=1:
+  elif row >=3 and row<= 1 or column   >= 3 or column <=1:
     print("Coordinates should be from 1 to 3!")
     continue
   else :
-    print("You should enter numbers!") 
+    print("column o u should enter numbers!") 
     continue
 
     
